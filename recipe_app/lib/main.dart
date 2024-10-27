f826f6b8804ca022eb39d3846cc77717027b2ee6
DevPathak503 <113939346+DevPathak1@users.noreply.github.com>
2024-10-27 11:43:23 -0700

    flutter to firebase connected

--- recipe_app/lib/main.dart
+++ recipe_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 const FirebaseOptions MyFirebaseOptions = FirebaseOptions(
   apiKey: "AIzaSyA0CBD0ZspPk7Kg25t4-zBpSxWtTpq4GEE",
@@ -14,13 +15,10 @@ const FirebaseOptions MyFirebaseOptions = FirebaseOptions(
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  try {
-    await Firebase.initializeApp(options: MyFirebaseOptions); 
-  } catch (e) {
-    print("Error initializing Firebase: $e");
-  }
+  await Firebase.initializeApp(options: MyFirebaseOptions); 
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -29,21 +27,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
@@ -65,11 +48,14 @@ class _MyHomePageState extends State<MyHomePage> {
   final _formKey = GlobalKey<FormState>();
   final TextEditingController _nameController = TextEditingController();
 
+  // Define a reference to the Firebase Realtime Database
+  final DatabaseReference ref = FirebaseDatabase.instance.ref();
+
   // Function to add name to Firebase
   void _submitName() {
     if (_formKey.currentState!.validate()) {
       String name = _nameController.text;
-      
+
       ref.child("users").push().set({"name": name}).then((_) {
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text("Name added to database!")),
